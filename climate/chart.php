379acf9188d3b4a8e6e7d7f6f5fcd82b35b3542d
Brendan M. Lowe <lowe75@hawkmail.newpaltz.edu>
2016-05-02 10:10:42 -0400

    I forgot to add this stuff before.

--- climate/chart.php
+++ climate/chart.php
@@ -13,8 +13,6 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
-		
-
 
 	</head>
 	<body>
